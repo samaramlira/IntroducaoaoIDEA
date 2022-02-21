325b9c283033f624b2d2ee82986f4998669827e0
samaramlira <samara.lira6@gmail.com>
2022-02-21 17:37:49 -0300

    Introdução ao IDEA

---
+++ Main.kt
@@ -0,0 +1,21 @@
+fun main() {
+
+    //Conversa 15 linhas
+
+    println("Fernando - Oi karen, tudo bem?")
+    println("Karen - Oi, quanto tempo, estou mais ou menos")
+    println("Fernando - Como assim? Me conte")
+    println("Karen - Estou meio parada, não sei o que fazer")
+    println("Fernando - Hmm, você tem preferência em alguma area?")
+    println("Karen - hmm estava pensando em algo na tecnologia, mas não sinto confiança")
+    println("Fernando - Por quê?")
+    println("Karen - Porque deve ser muito dificil, não tenho experiência e não sou boa em exatas")
+    println("Fernando - Aaaah, não se preocupe com isso, tenho uma boa indicação para você")
+    println("Karen - Sério? Me conta maaaiis")
+    println("Fernando - Tem uma empresa chamada Generation que disponibiliza cursos para a area de ")
+    println("Karen - Nossaa!!! Que maravilha, não conheço, onde posso me cadastrar?")
+    println("Fernando - No próprio site da Generation ou no instagram, vai ter alguns testes e pronto")
+    println("Karen - Nossa amigoo, muito obrigadaa, não sei o que seria da minha vida sem você!!!")
+    println("Fernando - Amigos servem para isso, estou sempre aqui quando precisar")
+    println(" *Karen conseguiu passar, se formou e hoje trabalha junto com Fernando* ")
+}
